dabbe938287aa830e1aa7bb95faacbb4d9f327e9
yash gupta <yashgupta1mole@gmail.com>
2023-07-31 17:07:38 +0530

    second

--- src/Components/GenericComonents/SingleArtical.jsx
+++ src/Components/GenericComonents/SingleArtical.jsx
@@ -4,6 +4,10 @@ import Top from "../Header/Top";
 import { NewsData } from "../Assets/NewsData";
 import './SingleAritcalStyle.css'
 import PostedBy from "./PostedBy";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import FilterStoriesData from "../FilterStoriesData";
 const SingleArtical = () => {
   const NewsArrayIndex = useParams();
@@ -26,11 +30,26 @@ const SingleArtical = () => {
         <Top />
 
       </div>
-    <div className="SingleAritcalBody">
+      <div className="middleContainer">
+        <div className="clapNShare">
+        <span className="clap1">
+        <img className="clap1Img" src="/images/rythm@2x.png" alt="not found" />
+        9.3K
+          </span> 
+          <span className="share">
+          <img className="shareImg" src="/images/share@2x.png" alt="not found" />
+          Share this article
+          </span>
+        </div>
+      <div className="SingleAritcalBody">
     <div className="SingleAritcalHeading">{data[Id].heading}</div>
     <div className="writer">
       <PostedBy/>
       <div className="socialMedia">
+        <FacebookIcon style={{color:'white',borderRadius:"5px", backgroundColor:"grey",marginRight:'10px',fontSize:"20px"}}/>
+        <TwitterIcon style={{color:'white',borderRadius:"5px", backgroundColor:"grey",marginRight:'10px',fontSize:"20px"}}/>
+        <InstagramIcon style={{color:'white',borderRadius:"5px", backgroundColor:"grey",marginRight:'10px',fontSize:"20px"}}/>
+        <YouTubeIcon style={{color:'white',borderRadius:"5px", backgroundColor:"grey",marginRight:'10px',fontSize:"20px"}}/>
       </div>
     </div>
     <img className="SingleAritcalImage" src={data[Id].images} alt="Not Found"/>
@@ -39,10 +58,16 @@ const SingleArtical = () => {
      {(wordsToShow!=-1)? <div onClick={TextView} className="showMore">⬇️ Show More</div>:<div onClick={TextView} className="showLess">⬆️ Show Less</div>}
       </div>
     <div className="writerInfo">
+    <span className="clap2">
+        <img className="clap1Img" src="/images/rythm@2x.png" alt="not found" />
+        9.3K claps
+          </span> 
     <PostedBy/>
     </div>
     
-    </div>
+    </div>  
+      </div>
+    
   <div className="MoreFromSiren">
     <FilterStoriesData type={"type6"} id={Id} />       
     <FilterStoriesData type={"type6"} id={Id+1} />       
